874face25750cea94d099bd5ff7e14c712c8e4a1
sea-rod <seam22rodr3@gmail.com>
2025-01-05 22:15:55 +0530

    add utils and lazy_load for proseccing docs and text

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from weaviate.classes.init import Auth
 from weaviate.classes.config import Property, DataType
 from transformers import AutoModel
 from langchain_community.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from utils import SentenceTextSplitter
+
 
 DATA_PATH = "./output_data"
 load_dotenv()
@@ -24,29 +25,18 @@ def generate_data_store():
     chunks = split_text(documents)
     save_to_weviate(chunks)
 
+def split_text(documents):
+    text_splitter = SentenceTextSplitter(
+        chunk_size=5,
+        chunk_overlap=1,
+    )
+    chunks = text_splitter.split_documents(documents)
+    return chunks
 
 def load_documents():
     loader = DirectoryLoader(DATA_PATH, glob="**/*.md", show_progress=True)
-    documents = loader.load()
+    documents = loader.lazy_load()
     return documents
-
-
-def split_text(documents):
-    print(len(documents),":len")
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=100,
-        length_function=len,
-        add_start_index=True,
-    )
-    chunks = text_splitter.split_documents(documents)
-    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
-
-    document = chunks[0]
-    print(document.page_content)
-    print(document.metadata)
-    return chunks
-
 
 def save_to_weviate(chunks):
     weaviate_url = os.environ["WEAVIATE_URL"]
@@ -63,16 +53,6 @@ def save_to_weviate(chunks):
         # Define Weaviate schema if not already done
         class_name = "PDFChunk"
         if not client.collections.exists(class_name):
-            # schema = {
-            #     "class": class_name,
-            #     "vectorizer": "none",  # Custom embeddings are provided
-            #     "properties": [
-            #         {"name": "content", "dataType": ["text"]},
-            #         {"name": "chunk_index", "dataType": ["int"]},
-            #         {"name": "user_id", "dataType": ["string"]},
-            #         {"name": "pdf_id", "dataType": ["string"]},
-            #     ],
-            # }
             client.collections.create(
                 class_name,
                 properties=[
@@ -88,14 +68,14 @@ def save_to_weviate(chunks):
         pdf_chunk = client.collections.get(class_name)
         for index, chunk in enumerate(chunks):
             # Generate the embedding
-            embedding = embedding_model.encode(chunk.page_content)
+            embedding = embedding_model.encode(chunk[0],device="cuda")
 
             # Metadata for the chunk
             metadata = {
-                "content": chunk.page_content,
+                "content": chunk,
                 "chunk_index": index,
                 "user_id": "user_id",  # Replace with actual user_id
-                "pdf_id": "pdf_id",  # Replace with actual pdf_id
+                "pdf_id": chunk[1],  # Replace with actual pdf_id
             }
 
             uuid = pdf_chunk.data.insert(metadata,vector=embedding)
@@ -103,10 +83,31 @@ def save_to_weviate(chunks):
         print("All chunks saved successfully!",uuid)
 
 
+def read_obj():
+    weaviate_url = os.environ["WEAVIATE_URL"]
+    weaviate_api_key = os.environ["WEAVIATE_API_KEY"]
+    with weaviate.connect_to_weaviate_cloud(
+        cluster_url=weaviate_url,
+        auth_credentials=Auth.api_key(weaviate_api_key),
+    ) as client:
+        collection = client.collections.get("PDFChunk")
+
+        for item in collection.iterator():
+            print(f"{item.uuid} {item.properties}...")
+
+
+def delete():
+    weaviate_url = os.environ["WEAVIATE_URL"]
+    weaviate_api_key = os.environ["WEAVIATE_API_KEY"]
+    with weaviate.connect_to_weaviate_cloud(
+        cluster_url=weaviate_url,
+        auth_credentials=Auth.api_key(weaviate_api_key),
+    ) as client:
+        client.collections.delete("PDFChunk")
+
 if __name__ == "__main__":
-    # extract_from_pdf()
-    # generate_data_store()
-    embedding_model = AutoModel.from_pretrained(
-        "jinaai/jina-embeddings-v2-base-en", trust_remote_code=True
-    )
-    print(embedding_model.encode("what is classication"))
+    extract_from_pdf()
+    generate_data_store()
+    read_obj()
+    # delete()
+
---
+++ utils.py
@@ -0,0 +1,21 @@
+from nltk.tokenize import sent_tokenize
+# nltk.download('punkt_tab')
+
+
+class SentenceTextSplitter:
+    def __init__(self, chunk_size=3, chunk_overlap=1):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+    def split_documents(self, docs):
+        text = ""
+        for doc in docs:
+            text += doc.page_content.strip("\n")
+            sentences = sent_tokenize(text)
+            for i in range(0, len(sentences), self.chunk_size - self.chunk_overlap):
+                chunk = sentences[i:i + self.chunk_size]
+                yield ' '.join(chunk)
+
+
+
+
